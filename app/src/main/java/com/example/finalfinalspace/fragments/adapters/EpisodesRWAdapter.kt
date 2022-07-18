33f319b381ab87cc8d486b57962bb803184052fd
Boris Engler <engler@ginasystem.com>
2022-07-18 15:30:47 +0200

    View binding

--- app/src/main/java/com/example/finalfinalspace/fragments/adapters/EpisodesRWAdapter.kt
+++ app/src/main/java/com/example/finalfinalspace/fragments/adapters/EpisodesRWAdapter.kt
@@ -8,6 +8,8 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.finalfinalspace.R
+import com.example.finalfinalspace.databinding.EpisodeCardviewBinding
+import com.example.finalfinalspace.databinding.FragmentEpisodeBinding
 import com.example.finalfinalspace.datamanagment.episodes.EpisodesInfo
 import javax.inject.Inject
 
@@ -24,19 +26,14 @@ class EpisodesRWAdapter @Inject constructor() :
         parent: ViewGroup,
         viewType: Int
     ): EpisodesRWAdapter.EpisodeViewHolder {
-        val inflater: LayoutInflater = LayoutInflater.from(parent.context)
-        val view: View = inflater.inflate(R.layout.episode_cardview, parent, false)
-        return EpisodeViewHolder(view, onListener)
+        return EpisodeViewHolder(EpisodeCardviewBinding.inflate(LayoutInflater.from(parent.context), parent, false), onListener)
     }
 
     override fun onBindViewHolder(holder: EpisodeViewHolder, position: Int) {
-        getItem(position)?.let { holder.bind(it) }
+        holder.bind(getItem(position))
     }
 
-    inner class EpisodeViewHolder(itemView: View, listener: OnItemClickListener) : RecyclerView.ViewHolder(itemView) {
-        var episodeNameView: TextView = itemView.findViewById(R.id.episode_name)
-        var episodeDateView: TextView = itemView.findViewById(R.id.episode_date)
-//        var episodeImage: ImageView = itemView.findViewById(R.id.episode_img)
+    inner class EpisodeViewHolder(private val binding: EpisodeCardviewBinding, listener: OnItemClickListener) : RecyclerView.ViewHolder(binding.root) {
 
         init {
             itemView.setOnClickListener {
@@ -45,8 +42,8 @@ class EpisodesRWAdapter @Inject constructor() :
         }
 
         fun bind(info: EpisodesInfo) {
-            episodeNameView.text = info.name
-            episodeDateView.text = info.airDate
+            binding.episodeName.text = info.name
+            binding.episodeDate.text = info.airDate
         }
     }
 
